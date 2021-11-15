01b11d19adfa7e576ae637388bceb7cdc3c70864
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-11-14 22:23:44 -0600

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -1,105 +1,71 @@
+const getEncouragement = document.getElementById('getEncouragement')
+const getInspiration = document.getElementById('getInspiration')
+const getHelp = document.getElementById('getHelp')
+const getParamSubmit = document.getElementById('getParamSubmit')
+const getQuerySubmit = document.getElementById('getQuerySubmit')
 
-function showMessage(input, message, type) {
-	
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	
-	input.className = type ? "success" : "error";
-	return type;
-}
+const paramInput = document.getElementById('param-input')
+const responseArea = document.getElementsByClassName('response-area')[0];
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
 
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
 
-function validateEmail(input, requiredMsg, invalidMsg) {
-	
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
-
-const form = document.querySelector("#signup");
-
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-
-form.addEventListener("submit", function (event) {
-	
-	event.preventDefault();
-
-	
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	
-	if (nameValid && emailValid) {
-		alert("You have subscribe successfully.");
-	}
+getEncouragement.addEventListener('click', () => {
+    let encouragement = {
+        message: `A woman is the full circle. Within her is the power to create, nurture and transform.`,
+        meaning: 'Diane Marechild'
+       }
+    axios
+        .post("http://localhost:4000/api/encouragement", encouragement)
+        .then(res => displayRes([res.data]))
 });
 
-const btnAdd = document.querySelector('#btnAdd');
-        const btnRemove = document.querySelector('#btnRemove');
-        const sb = document.querySelector('#list');
-        const name = document.querySelector('#name');
+getInspiration.addEventListener('click', () => {
+    let inspiration = {
+        message: `You're off to great places, today is your day. Your mountain is waiting, so get on your way.`,
+        meaning: 'Dr. Seuss'
+       }
+    axios
+        .post("http://localhost:4000/api/inspiration", inspiration)
+        .then(res => displayRes([res.data]))
+});
 
-        btnAdd.onclick = (e) => {
-            e.preventDefault();
+getHelp.addEventListener('click', () => {
+    let help = {
+        message: `People appreciate and never forget that helping hand especially when times are tough.`,
+        meaning: 'Catherine Pulsifer'
+       }
+    axios
+        .post("http://localhost:4000/api/help", help)
+        .then(res => displayRes([res.data]))
+});
 
-            // validate the option
-            if (name.value == '') {
-                alert('Please enter the name.');
-                return;
-            }
-            // create a new option
-            const option = new Option(name.value, name.value);
-            // add it to the list
-            sb.add(option, undefined);
+getQuerySubmit.addEventListener('click', () => {
+    axios
+   .delete(`http://localhost:4000/api/help/:message`)
+   .then(res => displayRes([res.data]))
+   
+});
 
-            // reset the value of the input
-            name.value = '';
-            name.focus();
-        };
-
-        // remove selected option
-        btnRemove.onclick = (e) => {
-            e.preventDefault();
-
-            // save the selected option
-            let selected = [];
-
-            for (let i = 0; i < sb.options.length; i++) {
-                selected[i] = sb.options[i].selected;
-            }
-
-            // remove all selected option
-            let index = sb.options.length;
-            while (index--) {
-                if (selected[index]) {
-                    sb.remove(index);
-                }
-            }
-        };
+getParamSubmit.addEventListener('click', () => {
+ 
+    let paramInput2 = window.prompt('Write your own affirmation')
+    console.log(paramInput2)
+    axios
+    .get(`http://localhost:4000/api/helpInput/`)
+    .then(res => displayRes([res.data]))
+});
 
 
-
+function displayRes(data){
+    responseArea.innerHTML = null;
+        data.forEach(item => {
+            const p = document.createElement('p')
+            const t = document.createTextNode(item)
+            p.appendChild(t);
+    
+             responseArea.appendChild(p)
+     })
+ }
+   
